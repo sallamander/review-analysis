d8e216bbfb45738f6538324d4d5278168634025c
sallamander <ssall@alumni.nd.edu>
2016-07-10 19:00:53 -0500

    Initialize model.py

---
+++ review_analysis/models/model.py
@@ -0,0 +1,16 @@
+"""A script for fitting recurrent networks for predicting on amazon reviews."""
+
+import pickle
+import numpy as np
+
+if __name__ == '__main__':
+    wrd_idx_dct_fp = 'work/wrd_idx_dct.pkl'
+    embedding_weights_fp = 'work/embedding_weights.npy'
+    vectorized_reviews_fp = 'work/vec_reviews.npy'
+    ratios_fp = 'work/reviews/amazon/filtered_ratios.npy'
+
+    with open(wrd_idx_dct_fp, 'rb') as f: 
+        wrd_idx_dct = pickle.load(f)
+    embedding_weights = np.load(embedding_weights_fp)
+    vectorized_reviews = np.load(vectorized_reviews_fp)
+    ratios = np.load(ratios_fp)
