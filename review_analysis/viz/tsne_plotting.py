daaa549b0292c1e895386612241c0019a2bbc0d3
sallamander <ssall@alumni.nd.edu>
2016-07-13 10:13:58 -0500

    Move tsne plotting into a class, fix tests for it
    Note that before this PR, a number of tests were broken (and some still
    are). This PR fixes tests associated with the newly built TSNEPlotter,
    as well as all tests in `test_tsne_plotting` **except**
    `test_gen_tsne_embedding`, because that will be moved into a class in a
    subsequent PR. This leaves one other test broken - the one in
    `test_eval_utils`.

--- review_analysis/viz/tsne_plotting.py
+++ review_analysis/viz/tsne_plotting.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from collections import defaultdict
 from nltk import pos_tag
+from sklearn.feature_extraction.text import CountVectorizer
 from tsne import tsne
 from review_analysis.utils.preprocessing import filter_ratios
 from review_analysis.utils.data_io import return_data
@@ -15,13 +16,13 @@ def filter_corpora(corpora, num_top_wrds):
 
     Args: 
     ----
-        corpora: dict
-            name (str): corpus (1d np.ndarray of strings) pairs
+        corpora: list of tuples
+            (name (str), corpus (1d np.ndarray of strings) pairs)
         num_top_words :int
 
     Return:
     ------
-        filtered_corpora: dict
+        filtered_corpora: list of tuples
     """
 
     vectorizer = CountVectorizer(max_features=num_top_wrds, stop_words='english')
@@ -39,14 +40,13 @@ def gen_wrd_vec_matrix(filtered_corpora, wrd_embedding):
 
     Args:
     ----
-        filtered_corpora: dict
-            name (str): filtered corpus (list of words) pairs
+        filtered_corpora: list of tuples 
+            (name (str), filtered corpus (list of words) pairs)
         wrd_embedding: gensim.models.word2vec.Word2Vec fitted model
 
     Return:
     ------
-        wrd_embedding_corpora: dict
-            name (str): word embedding (2d np.ndarray) pairs
+        wrd_embedding_corpora: list of tuples
     """
 
     wrd_embedding_corpora = [] 
@@ -84,86 +84,89 @@ def gen_tsne_embedding(wrd_embedding_corpora):
 
     return tsne_embedding_corpora
 
-def plot_tsne(tsne_embedding_corpora, filtered_corpora, pos_filter=None,
-              save_fp=None, title=None):
-    """Plot the tsne embeddings of the vocab in `filtered_corpora`.
+class TSNEPlotter(object):
+    """A class for building visualizations from TSNE embeddings.
 
     Args:
     ----
-        tsne_embedding_corpora:dict
+        tsne_embedding_corpora: dict
             name (str): tsne embedding (2d np.ndarray) pairs
-        filtered_corpora: dict
-            name (str): filtered corpus (list of words) pairs
-        pos_filter (optional): set (or other array-like)
-            parts of speech to filter the tsne plot by
-        save_fp (optional): str
-        title (optional): str
+        filtered_corpora: list of tuples
+            (name (str), filtered corpus (list of words) pairs)
+        pos_filters (optional): list 
+            parts of speech to also plot
+        save_dir (optional): str
     """
 
-    dim_mins, dim_maxes = calc_corpora_embedding_bounds(tsne_embedding_corpora)
+    def __init__(self, tsne_embedding_corpora, filtered_corpora, title=None,
+                 pos_filter=None, save_fp=None):
+        self.tsne_embedding_corpora = tsne_embedding_corpora
+        self.filtered_corpora = filtered_corpora
+        self.title = title
+        self.corpora_size = len(filtered_corpora)
+        self.pos_filter = {} if not pos_filter else pos_filter
+        self.save_fp = save_fp
+        self._build_plots()
 
-    plt.figure(figsize=(10, 6))
-    ax = plt.subplot(111)
-    ax.axis('off')
+    def _build_plots(self):
+        """Build the plot with the inputted embeddings and words."""
 
-    num = 0
-    corpora_size = len(filtered_corpora)
-    legend_handles = []
-    for name, corpus in filtered_corpora:
-        num += 1
-        tsne_embeddings = tsne_embedding_corpora[name]
-        tsne_embeddings = (tsne_embeddings - dim_mins) / (dim_maxes - dim_mins)
-        color = plt.cm.Set3(num / corpora_size)
-        patch = mpatches.Patch(color=color, label=name)
-        legend_handles.append(patch)
+        self._calc_corpora_embedding_bounds()
+        self._set_legend()
+        self._scale_tsne_embeddings()
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+        ax.axis('off')
+        for idx, corpus in enumerate(self.filtered_corpora):
+            name, wrds = corpus
+            tsne_embeddings = self.tsne_embedding_corpora[name]
+            color = self.legend_handles[idx].get_facecolor()
+            wrds_n_pos = pos_tag(wrds)
+            for tsne_embedding, wrd_n_pos in zip(tsne_embeddings, wrds_n_pos):
+                if wrd_n_pos[1] in self.pos_filter or not self.pos_filter:
+                    plt.text(tsne_embedding[0], tsne_embedding[1], wrd_n_pos[0], 
+                             fontdict={'weight': 'bold', 'size': 12}, 
+                             color=color)
+        plt.legend(loc='best', handles=self.legend_handles)
+        if self.title: 
+            plt.title(self.title)
+        if self.save_fp:
+            plt.savefig(self.save_fp)
         
-        if pos_filter: 
-            for tsne_embedding, wrd in zip(tsne_embeddings, corpus):
-                wrd_pos = pos_tag([wrd]) # Returns [(wrd, pos)]
-                if wrd_pos[0][1] in pos_filter: 
-                    plt.text(tsne_embedding[0], tsne_embedding[1], wrd, 
-                         color=color, fontdict={'weight': 'bold', 'size': 12})
-        else: 
-            for tsne_embedding, wrd in zip(tsne_embeddings, corpus):
-                plt.text(tsne_embedding[0], tsne_embedding[1], wrd, 
-                         color=color, fontdict={'weight': 'bold', 'size': 12})
-    
-    plt.legend(loc='best', handles=legend_handles)
-    if title: 
-        plt.title(title)
-    if save_fp:
-        plt.savefig(save_fp)
-    else: 
-        plt.show()
+    def _calc_corpora_embedding_bounds(self):
+        """Calculate the min/max values for each embedding dimension. 
 
-def calc_corpora_embedding_bounds(tsne_embedding_corpora):
-    """Calculate the min/max values for each embedding dimension. 
+        These will be used to scale the tsne embeddings so that the words all appear
+        tightly in the plot.
+        """
 
-    These will be used to scale the tsne embeddings so that the words all appear
-    tightly in the plot.
+        mins, maxes = [], [] 
+        for embedding in self.tsne_embedding_corpora.values():
+            mins.append(embedding.min(0).tolist())
+            maxes.append(embedding.max(0).tolist())
 
-    Args:
-    ----
-        tsne_embedding_corpora:dict
-            name (str): tsne embedding (2d np.ndarray) pairs
+        mins = np.array(mins)
+        maxes = np.array(maxes)
+        self.dim_mins = mins.min(0)
+        self.dim_maxes = maxes.max(0)
 
-    Return:
-    ------
-        dim_mins: 1d np.ndarray
-        dim_maxes: 1d np.ndarray
-    """
+    def _set_legend(self):
+        """Set up `self.legend_handles`."""
+        
+        self.legend_handles = []
+        for idx, corpus in enumerate(self.filtered_corpora, 1):
+            name = corpus[0]
+            color = plt.cm.Set3(idx / self.corpora_size)
+            patch = mpatches.Patch(color=color, label=name)
+            self.legend_handles.append(patch)
 
-    mins, maxes = [], [] 
-    for name, embedding in tsne_embedding_corpora.items():
-        mins.append(embedding.min(0).tolist())
-        maxes.append(embedding.max(0).tolist())
+    def _scale_tsne_embeddings(self): 
+        """Scale the tsne embeddings using `self.dim_mins/self.dim_maxes`"""
 
-    mins = np.array(mins)
-    maxes = np.array(maxes)
-    dim_mins = mins.min(0)
-    dim_maxes = maxes.max(0)
-
-    return dim_mins, dim_maxes
+        for name, embedding in self.tsne_embedding_corpora.items():
+            embedding = ((embedding - self.dim_mins) / 
+                         (self.dim_maxes - self.dim_mins))
+            self.tsne_embedding_corpora[name] = embedding
 
 if __name__ == '__main__':
     # Loading the entire df will allow for throwing the raw texts into 
@@ -189,24 +192,23 @@ if __name__ == '__main__':
                ('Middle Reviews (0.10 - 0.90)', middle_reviews), 
                ('Helpful Reviews (0.90 - 1.00)', helpful_reviews)]
     num_top_wrds = 100
-
     filtered_corpora = filter_corpora(corpora, num_top_wrds)
     wrd_embedding_corpora = gen_wrd_vec_matrix(filtered_corpora, wrd_embedding)
     tsne_embedding_corpora = gen_tsne_embedding(wrd_embedding_corpora)
     
     title = 'Word Embeddings for the Top {} Words In Reviews'.format(num_top_wrds)
-    save_fp = 'work/tsne_{}.png'.format(num_top_wrds)
-    plot_tsne(tsne_embedding_corpora, filtered_corpora, save_fp=save_fp, 
-              title=title)
-    
-    pos_tags = [('Adjectives', {'JJ', 'JJR', 'JJS'}), 
-                ('Nouns', {'NN', 'NNP', 'NNPS', 'NNS'}), 
-                ('Pronouns', {'PRP', 'PRP$'}), ('Adverbs', {'RB', 'RBR', 'RBS'}), 
-                ('Verbs', {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'})]
+    save_fp = 'work/viz/tsne_{}.png'.format(num_top_wrds)
+    tsne_plotter = TSNEPlotter(tsne_embedding_corpora, filtered_corpora,           
+                               save_fp=save_fp, title=title)
+
     title = 'Word Embeddings for the {} in Top {} Words in Reviews'
-    save_fp = 'work/tsne_{}_{}.png'
-    for descriptor, tags in pos_tags:
-        tag_title = title.format(descriptor, num_top_wrds)
-        tag_save_fp = save_fp.format(descriptor, num_top_wrds)
-        plot_tsne(tsne_embedding_corpora, filtered_corpora, pos_filter=tags, 
-                  save_fp=tag_save_fp, title=tag_title)
+    pos_tags = {'Adjectives': {'JJ', 'JJR', 'JJS'},
+               'Nouns': {'NN', 'NNP', 'NNPS', 'NNS'}, 
+               'Adverbs': {'RB', 'RBR', 'RBS'}, 
+               'Verbs': {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}}
+    save_fp = 'work/viz/tsne_{}_{}.png'
+    for descriptor, tags in pos_tags.items():
+            tag_title = title.format(descriptor, num_top_wrds)
+            tag_save_fp = save_fp.format(descriptor, num_top_wrds)
+            TSNEPlotter(tsne_embedding_corpora, filtered_corpora, pos_filter=tags, 
+                      save_fp=tag_save_fp, title=tag_title)
--- tests/viz/test_tsne_plotting.py
+++ tests/viz/test_tsne_plotting.py
@@ -1,17 +1,43 @@
 import pytest
 import numpy as np
 from gensim.models.word2vec import Word2Vec
-from review_analysis.viz.word_plotting import filter_corpora, \
-        gen_wrd_vec_matrix, gen_tsne_embedding, calc_corpora_embedding_bounds
+from review_analysis.viz.tsne_plotting import filter_corpora, \
+        gen_wrd_vec_matrix, gen_tsne_embedding, TSNEPlotter 
 
 class TestWordPlotting: 
+
+    def setup_class(cls):
+
+        cls.embedding_corpora = {'Test1': np.zeros((2, 10)), 
+                                 'Test2': np.ones((5, 10)), 
+                                 'Test3': np.random.random((10, 10))}
+        cls.filtered_corpora = [('Test1', ['This' ,'is', 'a', 'list']), 
+                                ('Test2', ['Hi', 'Matthew', '??']), 
+                                ('Test3', ['Cant', 'forget', 'Felix', '!'])]
+
+    def teardown_class(cls):
+       del cls.embedding_corpora, cls.filtered_corpora
+
+    def test_calc_corpora_embedding_bounds(self):
+
+        tsne_plotter = TSNEPlotter(self.embedding_corpora, self.filtered_corpora)
+
+        assert np.all(tsne_plotter.dim_mins == 0)
+        assert np.all(tsne_plotter.dim_maxes == 1)
+
+    def test_scale_tsne_embeddings(self): 
+
+        tsne_plotter = TSNEPlotter(self.embedding_corpora, self.filtered_corpora)
+        for embedding in tsne_plotter.tsne_embedding_corpora.values():
+            assert (embedding.min() >= 0)
+            assert (embedding.max() <= 1)
 
     def test_filter_corpora(self):
         corpus1 = np.array(['This is the first corpus...', 
                              'There are only two documents.', 
                              'Pysche!'])
         corpus2 = np.array(['Now there is a second corpus!'])
-        corpora = {'corpus1': corpus1, 'corpus2': corpus2}
+        corpora = [('corpus1', corpus1), ('corpus2', corpus2)]
         
         num_top_wrds = 2
         filtered_corpora = filter_corpora(corpora, num_top_wrds)
@@ -19,15 +45,15 @@ class TestWordPlotting:
         # <= because stopwords are removed, which may not leave enough words
         # in the text examples to hit `num_top_wrds`
         assert (len(filtered_corpora) == len(corpora))
-        assert (len(filtered_corpora['corpus1']) <= num_top_wrds)
-        assert (len(filtered_corpora['corpus2']) <= num_top_wrds)
+        assert (len(filtered_corpora[0][1]) <= num_top_wrds)
+        assert (len(filtered_corpora[1][1]) <= num_top_wrds)
 
         num_top_wrds = 5
         filtered_corpora = filter_corpora(corpora, num_top_wrds)
 
         assert (len(filtered_corpora) == len(corpora))
-        assert (len(filtered_corpora['corpus1']) <= num_top_wrds)
-        assert (len(filtered_corpora['corpus2']) <= num_top_wrds)
+        assert (len(filtered_corpora[0][1]) <= num_top_wrds)
+        assert (len(filtered_corpora[1][1])<= num_top_wrds)
 
     def test_gen_wrd_vec_matrix(self):
         
@@ -35,15 +61,15 @@ class TestWordPlotting:
         filtered_corpus2 = ['two', 'for', 'Dan']
         sentences = [filtered_corpus1, filtered_corpus2]
         vocab = set(word for sentence in sentences for word in sentence)
-
-        filtered_corpora = {'corpus1' : filtered_corpus1, 
-                            'corpus2' : filtered_corpus2}
+ 
+        filtered_corpora = [('corpus1' , filtered_corpus1), 
+                            ('corpus2' , filtered_corpus2)]
         wrd_embedding = Word2Vec(sentences, min_count=1, size=len(vocab))
         embed_dim = wrd_embedding.vector_size
         wrd_matrix_corpora = gen_wrd_vec_matrix(filtered_corpora, wrd_embedding)
 
         assert (len(wrd_matrix_corpora) == len(filtered_corpora))
-        assert (wrd_matrix_corpora['corpus1'].shape == 
+        assert (wrd_matrix_corpora[0][1].shape == 
                 (len(filtered_corpus1), embed_dim))
 
     def test_gen_tsne_embedding(self):
@@ -57,16 +83,3 @@ class TestWordPlotting:
         assert (len(tsne_embedding_corpora) == len(wrd_embedding_corpora))
         assert (tsne_embedding_corpora['corpus1'].shape == ((10, 2)))
         assert (tsne_embedding_corpora['corpus2'].shape == ((20, 2)))
-
-    def test_calc_corpora_embedding_bounds(self): 
-
-        embedding_corpus1 = np.array([[1.5, 2.7]])
-        embedding_corpus2 = np.array([[1.3, 3.9]])
-        embedding_corpus3 = np.array([[1.7, 1.5]])
-        tsne_embedding_corpora = {'corpus1': embedding_corpus1, 
-                                  'corpus2': embedding_corpus2, 
-                                  'corpus3': embedding_corpus3}
-
-        dim_mins, dim_maxes = calc_corpora_embedding_bounds(tsne_embedding_corpora)
-        assert np.all(dim_mins == np.array([1.3, 1.5]))
-        assert np.all(dim_maxes == np.array([1.7, 3.9]))
