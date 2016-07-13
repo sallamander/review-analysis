cebbf26ca02b3c2cb4951c96e12a0fd195e9bb66
sallamander <ssall@alumni.nd.edu>
2016-07-13 16:59:09 -0500

    Add user friendly func. for confusion mat

--- review_analysis/utils/eval_utils.py
+++ review_analysis/utils/eval_utils.py
@@ -1,6 +1,7 @@
 """A module for aiding in evaluating the results of models."""
 
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 def intellegently_guess(ratios, classification=False, pred=None): 
     """Simulate the error from intellegently guessing (e.g. guessing the mean).
@@ -31,3 +32,27 @@ def intellegently_guess(ratios, classification=False, pred=None):
         error = np.sum((ratios - pred) ** 2) / nobs
 
     return error
+
+def get_confusion_mat(y_true, y_pred):
+    """Use sklearns `confusion_matrix` to return a user friendly confusion mat.
+
+    Args:
+    ----
+        y_true: 1d np.ndarray
+        y_pred: 1d np.ndarray
+
+    Return:
+    ------
+        confusion_dct: dct
+            cell_label (str) : n_obs (int) key-value pairs 
+    """ 
+
+    confusion_mat = confusion_matrix(y_true, y_pred)
+    
+    tn, tp = confusion_mat[0][0], confusion_mat[1][1]
+    fn, fp = confusion_mat[1][0], confusion_mat[0][1] 
+
+    confusion_dct = {'true_negatives': tn, 'true_positives': tp, 
+                     'false_negatives': fn, 'false_positives': fp}
+    
+    return confusion_dct
--- tests/utils/test_eval_utils.py
+++ tests/utils/test_eval_utils.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
-from review_analysis.utils.eval_utils import intellegently_guess
+from review_analysis.utils.eval_utils import intellegently_guess, \
+        get_confusion_mat
 
 class TestEvalUtils: 
 
@@ -23,3 +24,23 @@ class TestEvalUtils:
         ratios = np.array([[0.01], [0.05], [0.12]])
         error = intellegently_guess(ratios, pred=0.03)
         assert abs(error - 0.0089 / 3) < eps
+
+    def test_get_confusion_mat(self):
+
+        y_true = [0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
+        y_pred = [0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+
+        confusion_dct = get_confusion_mat(y_true, y_pred)
+        assert (confusion_dct['true_negatives'] == 1)
+        assert (confusion_dct['true_positives'] == 2)
+        assert (confusion_dct['false_positives'] == 3)
+        assert (confusion_dct['false_negatives'] == 4)
+
+        y_true = [1, 0, 1, 1, 0, 1, 1, 0, 1, 0]
+        y_pred = [0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+
+        confusion_dct = get_confusion_mat(y_true, y_pred)
+        assert (confusion_dct['true_negatives'] == 2)
+        assert (confusion_dct['true_positives'] == 3)
+        assert (confusion_dct['false_positives'] == 2)
+        assert (confusion_dct['false_negatives'] == 3)
