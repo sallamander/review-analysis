f819ad1b2cd5dfe3fbf7e17ed4fd9b589220a8ee
sallamander <ssall@alumni.nd.edu>
2016-07-14 04:37:29 -0500

    Move confusion matrix calculations into a class

--- review_analysis/utils/eval_utils.py
+++ review_analysis/utils/eval_utils.py
@@ -33,26 +33,84 @@ def intellegently_guess(ratios, classification=False, pred=None):
 
     return error
 
-def get_confusion_mat(y_true, y_pred):
-    """Use sklearns `confusion_matrix` to return a user friendly confusion mat.
+class ConfusionMatrix(object): 
+    """A utility class for working with a confusion mat and associated stats."""
 
-    Args:
-    ----
-        y_true: 1d np.ndarray
-        y_pred: 1d np.ndarray
+    def fit(self, y_true, y_pred): 
+        """Store the inputted obs. and generate a confusion matrix.
 
-    Return:
-    ------
-        confusion_dct: dct
-            cell_label (str) : n_obs (int) key-value pairs 
-    """ 
+        Args:
+        ----
+            y_true: 1d np.ndarray
+            y_pred: 1d np.ndarray
+        """
 
-    confusion_mat = confusion_matrix(y_true, y_pred)
-    
-    tn, tp = confusion_mat[0][0], confusion_mat[1][1]
-    fn, fp = confusion_mat[1][0], confusion_mat[0][1] 
+        self.y_true, self.y_pred = y_true, y_pred
+        self.confusion_mat = confusion_matrix(y_true, y_pred)
 
-    confusion_dct = {'true_negatives': tn, 'true_positives': tp, 
-                     'false_negatives': fn, 'false_positives': fp}
-    
-    return confusion_dct
+        tn, tp = self.confusion_mat[0][0], self.confusion_mat[1][1]
+        fn, fp = self.confusion_mat[1][0], self.confusion_mat[0][1] 
+        self.confusion_dct = {'true_negatives': tn, 'true_positives': tp, 
+                              'false_negatives': fn, 'false_positives': fp}
+
+    def get_cell_counts(self, subsets=None):
+        """Return back a user friendly version/subset of the confusion matrix.
+
+        Args:
+        ----
+            subsets (optional): list or other iterable
+                iterable of one or more of <true/false>_<positives/negatives>
+
+        Return:
+        ------
+            dictionary of cell_label (str) : n_obs (int) pairs
+        """
+
+        if not hasattr(self, 'confusion_mat'): 
+            raise RuntimeError("Must call `fit` method of Confucious first!")
+        
+        confusion_counts = {}
+        if subsets: 
+            for subset in subsets:
+                confusion_counts[subset] = self.confusion_dct[subset]
+            return confusion_counts
+        else: 
+            return self.confusion_dct
+
+    def get_cell_obs(self, subsets=None):
+        """Generate a mask for obs. from given cells of the confusion matrix.
+
+        Args:
+        ----
+            subsets(optional): list or other iteratble
+                iterable of one or more of <true/false>_<positives/negatives>
+
+        Return:
+        ------
+            confusion_obs: dct
+                cell_label (str): obs (1d np.ndarray) pairs
+        """
+            
+        if not hasattr(self, 'confusion_mat'): 
+            raise RuntimeError("Must call `fit` method of Confucious first!")
+
+        correct_mask = (self.y_true == self.y_pred)
+        incorrect_mask = (self.y_true != self.y_pred)
+
+        tp_mask = np.logical_and(correct_mask, self.y_true == 1)
+        tn_mask = np.logical_and(correct_mask, self.y_true == 0)
+        fn_mask = np.logical_and(incorrect_mask, self.y_true == 1)
+        fp_mask = np.logical_and(incorrect_mask, self.y_true == 0)
+
+        confusion_mask_dct = {'true_negatives': tn_mask, 
+                              'true_positives': tp_mask, 
+                              'false_negatives': fn_mask, 
+                              'false_positives': fp_mask}
+        
+        confusion_obs = {} if subsets else confusion_mask_dct
+        if subsets: 
+            for subset in subsets:
+                confusion_obs[subset] = confusion_mask_dct[subset]
+            return confusion_obs
+        else: 
+            return confusion_obs 
--- tests/utils/test_eval_utils.py
+++ tests/utils/test_eval_utils.py
@@ -1,9 +1,22 @@
 import pytest
 import numpy as np
 from review_analysis.utils.eval_utils import intellegently_guess, \
-        get_confusion_mat
+        ConfusionMatrix
+        
 
 class TestEvalUtils: 
+
+    def setup_class(cls):
+
+        y_true = np.array([0, 1, 1, 0, 0, 0, 1, 1, 1, 1])
+        y_pred = np.array([0, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+
+        cls.confucius = ConfusionMatrix()
+        cls.confucius.fit(y_true, y_pred)
+
+    def teardown_class(cls):
+
+        del cls.confucius
 
     def test_intellegently_guess(self): 
         eps = 1e-3
@@ -25,22 +38,47 @@ class TestEvalUtils:
         error = intellegently_guess(ratios, pred=0.03)
         assert abs(error - 0.0089 / 3) < eps
 
-    def test_get_confusion_mat(self):
+    def test_fit(self):
 
-        y_true = [0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
-        y_pred = [0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+        assert (self.confucius.y_true is not None)
+        assert (self.confucius.y_pred is not None)
+        assert (self.confucius.confusion_dct is not None)
+        assert (len(self.confucius.confusion_dct) == 4)
 
-        confusion_dct = get_confusion_mat(y_true, y_pred)
+    def test_get_cell_counts(self): 
+
+        confusion_dct = self.confucius.get_cell_counts()
+
         assert (confusion_dct['true_negatives'] == 1)
         assert (confusion_dct['true_positives'] == 2)
         assert (confusion_dct['false_positives'] == 3)
         assert (confusion_dct['false_negatives'] == 4)
 
-        y_true = [1, 0, 1, 1, 0, 1, 1, 0, 1, 0]
-        y_pred = [0, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+        subsets = ['true_negatives', 'true_positives']
+        confusion_dct = self.confucius.get_cell_counts(subsets)
 
-        confusion_dct = get_confusion_mat(y_true, y_pred)
-        assert (confusion_dct['true_negatives'] == 2)
-        assert (confusion_dct['true_positives'] == 3)
-        assert (confusion_dct['false_positives'] == 2)
-        assert (confusion_dct['false_negatives'] == 3)
+        assert (len(confusion_dct) == 2)
+        assert (confusion_dct['true_negatives'] == 1)
+        assert (confusion_dct['true_positives'] == 2)
+
+    def test_get_confusion_obs(self): 
+
+        confusion_obs = self.confucius.get_cell_obs()
+            
+        assert (len(confusion_obs['true_negatives']) == 10)
+        assert (len(confusion_obs['true_positives']) == 10)
+        assert (len(confusion_obs['false_positives']) == 10)
+        assert (len(confusion_obs['false_negatives']) == 10)
+        assert (sum(confusion_obs['true_negatives']) == 1)
+        assert (sum(confusion_obs['true_positives']) == 2)
+        assert (sum(confusion_obs['false_positives']) == 3)
+        assert (sum(confusion_obs['false_negatives']) == 4)
+
+        subsets = ['true_negatives', 'true_positives']
+        confusion_obs = self.confucius.get_cell_obs(subsets)
+
+        assert (len(confusion_obs) == 2)
+        assert (len(confusion_obs['true_negatives']) == 10)
+        assert (len(confusion_obs['true_positives']) == 10)
+        assert (sum(confusion_obs['true_negatives']) == 1)
+        assert (sum(confusion_obs['true_positives']) == 2)
